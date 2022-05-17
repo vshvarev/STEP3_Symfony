4acf899c6e5f425592347c903f46ae772da8c4d5
shvarev <v.m.shvarev@yandex.ru>
2022-05-17 17:26:43 +0300

    Add new show route in MovieSessionController and show.html.twig template

--- src/Controller/MovieSessionController.php
+++ src/Controller/MovieSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\MovieSession;
 use App\Repository\MovieSessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,19 @@ use Twig\Environment;
 
 class MovieSessionController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie_session')]
+    #[Route('/movie', name: 'movieSession_list')]
     public function index(Environment $twig, MovieSessionRepository $movieSessionRepository): Response
     {
         return new Response($twig->render('movie_session/index.html.twig', [
             'movieSessions' => $movieSessionRepository->findAll(),
+        ]));
+    }
+
+    #[Route('/movie/{id}', name: 'movie')]
+    public function show(Environment $twig, MovieSession $movieSession): Response
+    {
+        return new Response($twig->render('movie_session/show.html.twig', [
+            'movieSession' => $movieSession,
         ]));
     }
 }
